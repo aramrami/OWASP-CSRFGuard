3fe47dcb788f7d74694286a6e9554ca6ffc76785
Pitukh Andrii <andriy.pitukh@gmail.com>
2015-10-31 15:20:58 +0200

    #28 deleted old IE6-IE7 anti-leaking functionality

--- csrfguard-test/src/main/webapp/script/csrfguard.js
+++ csrfguard-test/src/main/webapp/script/csrfguard.js
@@ -37,43 +37,16 @@
 	function addEvent( obj, type, fn ) {
 	    if (obj.addEventListener) {
 	        obj.addEventListener( type, fn, false );
-	        EventCache.add(obj, type, fn);
 	    }
 	    else if (obj.attachEvent) {
 	        obj["e"+type+fn] = fn;
 	        obj[type+fn] = function() { obj["e"+type+fn]( window.event ); }
 	        obj.attachEvent( "on"+type, obj[type+fn] );
-	        EventCache.add(obj, type, fn);
 	    }
 	    else {
 	        obj["on"+type] = obj["e"+type+fn];
 	    }
 	}
-	
-	var EventCache = function(){
-	    var listEvents = [];
-	    return {
-	        listEvents : listEvents,
-	        add : function(node, sEventName, fHandler){
-	            listEvents.push(arguments);
-	        },
-	        flush : function(){
-	            var i, item;
-	            for(i = listEvents.length - 1; i >= 0; i = i - 1){
-	                item = listEvents[i];
-	                if(item[0].removeEventListener){
-	                    item[0].removeEventListener(item[1], item[2], item[3]);
-	                };
-	                if(item[1].substring(0, 2) != "on"){
-	                    item[1] = "on" + item[1];
-	                };
-	                if(item[0].detachEvent){
-	                    item[0].detachEvent(item[1], item[2]);
-	                };
-	            };
-	        }
-	    };
-	}();
 	
 	/** string utility functions **/
 	String.prototype.startsWith = function(prefix) {
@@ -404,7 +377,6 @@
 		}
 		
 		/** update nodes in DOM after load **/
-		addEvent(window,'unload',EventCache.flush);
 		addEvent(window,'load', function() {
 			injectTokens("%TOKEN_NAME%", "%TOKEN_VALUE%");
 		});
