77eac4be7ad1eed38055e500c7f279b21fbb0efa
Doukkalli <azzeddine.ramrami@gmail.com>
2020-04-23 21:48:20 +0200

    Create HelloBean.java

---
+++ JavaServerFaces/src/main/java/com/mkyong/common/HelloBean.java
@@ -0,0 +1,24 @@
+package com.mkyong.common;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import java.io.Serializable;
+
+@ManagedBean
+@SessionScoped
+public class HelloBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
