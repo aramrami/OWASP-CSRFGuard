bb3421ed14eca73107bd83ea6ee05efe0880b222
forgedhallpass <13679401+forgedhallpass@users.noreply.github.com>
2020-07-30 18:15:00 +0300

    Created proper multi-module maven structure: * unified naming conventions for sub-modules * added possibility to deploy the test JSP application from maven using a profile * removed unnecessary files (the project should be IDE agnostic)
    Changed Language Level to Java 1.8
    
    Fixes issue #128

--- csrfguard/src/main/java/org/owasp/csrfguard/config/overlay/ConfigPropertiesCascadeCommonUtils.java
+++ csrfguard/src/main/java/org/owasp/csrfguard/config/overlay/ConfigPropertiesCascadeCommonUtils.java
@@ -2578,8 +2578,8 @@ public class ConfigPropertiesCascadeCommonUtils  {
     if (objectOrArrayOrCollection instanceof Collection) {
       Collection collection = (Collection) objectOrArrayOrCollection;
       Object first = collection.iterator().next();
-      return toArray(collection, first == null ? Object.class : first
-          .getClass());
+      final Class theClass = first == null ? Object.class : first.getClass();
+      return toArray(collection, theClass);
     }
     // make an array of the type of object passed in, size one
     Object array = Array.newInstance(objectOrArrayOrCollection.getClass(),
